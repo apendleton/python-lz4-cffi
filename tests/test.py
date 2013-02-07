c0ad669589c5ec9f81aeec9fe4c74deb621143b3
Andrew Pendleton <apendleton@sunlightfoundation.com>
2013-02-07 14:08:23 -0500

    Oops, API differences broke the test.

--- tests/test.py
+++ tests/test.py
@@ -2,4 +2,4 @@ import lz4
 import sys
 
 DATA = open("/dev/urandom", "rb").read(128 * 1024)  # Read 128kb
-sys.exit(DATA != lz4.loads(lz4.dumps(DATA)) and 1 or 0)
+sys.exit(DATA != lz4.uncompress(lz4.compress(DATA)) and 1 or 0)
